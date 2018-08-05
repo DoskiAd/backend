fc2dabdeeededf5f8fc6f0c717d98290ee2903ab
Hanomi <freefpdie@gmail.com>
2018-08-05 21:10:36 +0700

    media type - jpg

--- src/main/java/io/shifu/doskiad/controller/PhotoController.java
+++ src/main/java/io/shifu/doskiad/controller/PhotoController.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +23,13 @@ public class PhotoController {
 		this.servletContext = servletContext;
 	}
 
-	@RequestMapping(value = "items/{id}/photo/{file:.+}", method = RequestMethod.GET)
+	@RequestMapping(value = "items/{id}/photo/{file}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
 	public ResponseEntity<Resource> getImageAsResource(
 			@PathVariable("id") Long id,
-			@PathVariable("file") String file) {	    
-	    Resource resource = 
-	        new ServletContextResource(servletContext, 
-	        		String.format("/WEB-INF/photos/%s/%s", id, file));
+			@PathVariable("file") String file) {
+	    Resource resource =
+	        new ServletContextResource(servletContext,
+	        		String.format("/WEB-INF/photos/%s/%s.jpg", id, file));
 	    return new ResponseEntity<>(resource, HttpStatus.OK);
 	}
 
